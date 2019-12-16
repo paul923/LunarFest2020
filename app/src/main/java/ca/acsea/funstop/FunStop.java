cd3642772f85044696116d65503970cb94e9d8d7
sangwookYun <brtm0533@naver.com>
2019-12-16 15:49:15 -0800

    myPoint, about

--- app/src/main/java/ca/acsea/funstop/FunStop.java
+++ app/src/main/java/ca/acsea/funstop/FunStop.java
@@ -5,22 +5,46 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class FunStop extends Fragment {
+    Button btnStart;
+    FragmentManager fragmentManager;
+    FragmentTransaction transaction2;
+    FunStopSub funstopSub;
+
     public FunStop() {
         // Required empty public constructor
+    }
+    public FunStop(FragmentManager fm) {
+        fragmentManager=fm;
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        funstopSub=new FunStopSub();
+        View view=inflater.inflate(R.layout.fragment_fun_stop, container, false);
+
+        transaction2=fragmentManager.beginTransaction();
+
+        btnStart=(Button)view.findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                transaction2.replace(R.id.frameLayout, funstopSub).addToBackStack("tag2").commit();
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fun_stop, container, false);
+        return view;
     }
 }
